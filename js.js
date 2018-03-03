1e082d673f1484427df8c39369591dfc873e05ab
yuchiko <iyuchiko@gmail.com>
2018-03-03 13:28:36 +0200

    add scroll

---
+++ js.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var button = document.getElementById('js-next');
+var wrapper = document.querySelector('.wrapper-inner');
+var st = window.getComputedStyle(wrapper, null);
+
+function getTransformPlace() {
+  var tr = st.getPropertyValue("-webkit-transform") ||
+    st.getPropertyValue("-moz-transform") ||
+    st.getPropertyValue("-ms-transform") ||
+    st.getPropertyValue("-o-transform") ||
+    st.getPropertyValue("transform") ||
+    "Either no transform set, or browser doesn't do getComputedStyle";
+
+    return tr;
+}
+
+function getTransformV() {
+  var matrix = getTransformPlace();
+  return matrix.split(',')[5].slice(0,-1);
+}
+
+
+button.addEventListener('click', ()=>{
+  var currentPosition = getTransformV();
+  currentPosition =- window.innerHeight;
+  wrapper.style.transform = 'matrix(1,0,0,1,0,' + currentPosition + ')';
+});
